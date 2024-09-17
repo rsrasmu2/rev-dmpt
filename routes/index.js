88c09117817e6600cfcb4270e5c47fedd8311403
Robert Rasmussen <bobbyras90@gmail.com>
2024-09-16 17:35:06 -0700

    Attempt to connect to db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.start().then(() => {
+    res.render('index', { title: 'Express' });  
+  }).catch(error => {
+    console.error(error);
+    res.status(500).send('Error triggering start() function');
+  });
 });
 
 module.exports = router;
